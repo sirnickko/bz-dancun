acf032d1df808252b6089d830514e600b908736b
sirnickko <ogarinicholas@gmail.com>
2025-05-12 15:12:32 +0300

    Added loding rooms

--- Hotel Management.cpp
+++ Hotel Management.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<fstream>
 #include<vector>
+#include<sstream>
 
 using namespace std;
 
@@ -48,16 +49,55 @@ void displaystatus() const{
 // saving the room to the file 
 //Converts room data into a single line string for saving in file.
 
-string tofilestring() const{
+string toFileString() const{
     return to_string(room_number)+" "+ to_string(is_booked)+" "+customer_name+ "\n";
 
 }
-
-
-
-
-
-
-
+//loading the room from the file
+static room fromFileString(const string& line) {
+    int num;
+    bool booked;
+    string name;
+    istringstream iss(line);
+    iss >> num >> booked;
+    getline(iss, name);
+    if (!name.empty() && name[0] == ' ') name = name.substr(1);  // remove leading space
+    return room(num, booked, name);
+}
+//saving all rooms to room.txt
+void saverooms(const vector<room> & rooms){
+    ofstream out("room.txt");
+    for ( const room& r:rooms){
+        out << r.toFileString();
+    }
+    out.close();
 
 }
+//load all rooms from text file
+vector<room> loadRooms(int totalRooms) {
+    vector<room> rooms;
+    ifstream in("rooms.txt");
+    if (in) {
+        string line;
+        while (getline(in, line)) {
+            rooms.push_back(room::fromFileString(line));
+        }
+        in.close();
+    } else {
+        for (int i = 1; i <= totalRooms; ++i) {
+            rooms.push_back(room(i));
+        }
+    }
+    return rooms;
+}
+
+
+
+
+
+
+
+
+
+
+};
